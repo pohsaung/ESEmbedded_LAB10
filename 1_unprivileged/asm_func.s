e338c7bf815c40940af496d0f5c0e9b59826828c
pohsaung <pohsiang17582953@gmail.com>
2019-06-05 08:52:01 +0800

    hand1

--- 1_unprivileged/asm_func.s
+++ 1_unprivileged/asm_func.s
@@ -2,24 +2,44 @@
 
 .global	read_sp
 read_sp:
-	??????
+	mov r0 ,sp
+	isb
+	bx lr  
 
 .global	read_msp
 read_msp:
-	??????
-
+	mov r0, msp
+	isb
+	bx lr
 .global	read_psp
 read_psp:
-	??????
-
+	mov r0, psp
+    bx lr
 .global	read_ctrl
 read_ctrl:
-	??????
+	mrs r0, ctrl
+    bx lr
+.global	start_user
+start_user:
+	mov lr, r0
+	mov psp, r1
+	mov r0, 
+	bx lr
+
 
 .global	start_user
 start_user:
-	??????
+	mov lr,r0	
+	msr psp,r1  
+	mov r0,0b11    
+	msr control,r0 
+	isb
+	blx lr
+
 
 .global	sw_priv
 sw_priv:
-	??????
+	mov r0, 0b10
+	msr control, r0
+	isb
+	bx lr
